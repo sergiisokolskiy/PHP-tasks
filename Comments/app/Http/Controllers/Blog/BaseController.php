112f94e894bd66f1b333a0ecb22a4cae25ea06b6
Serhii Sokolskiy <33714147+sergiisokolskiy@users.noreply.github.com>
2022-05-04 17:08:15 +0300

    Create BaseController.php

---
+++ Comments/app/Http/Controllers/Blog/BaseController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Blog;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+abstract class BaseController extends Controller
+{
+    /** BaseController constructor */
+
+    public function  __construct()
+    {
+// Инициализация общих моментов для админки
+    }
+}
