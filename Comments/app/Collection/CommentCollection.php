01a4bd2c161782a5804494a09fc555383fba37f0
Serhii Sokolskiy <33714147+sergiisokolskiy@users.noreply.github.com>
2022-05-04 17:02:46 +0300

    Create CommentCollection.php

---
+++ Comments/app/Collection/CommentCollection.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Collection;
+use Illuminate\Database\Eloquent\Collection;
+
+class CommentCollection extends Collection
+{
+public function threaded()
+{
+$comments = parent::groupBy('parent_id');
+
+if (count($comments)) {
+$comments['root'] = $comments[''];
+unset($comments['']);
+}
+return $comments;
+}
+}
